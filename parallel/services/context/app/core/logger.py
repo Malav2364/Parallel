c6dadd362cc87c653526896788ca466af151b84f
Malav2364 <2303031057065@paruluniversity.ac.in>
2026-08-07 15:13:40 +0530

    Context services initialization

---
+++ parallel/services/context/app/core/logger.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+from pythonjsonlogger.json import JsonFormatter
+
+logger = logging.getLogger("context")
+
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+
+formatter = JsonFormatter(
+    "%(asctime)s %(levelname)s %(message)s"
+)
+
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
